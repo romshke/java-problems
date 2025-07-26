3dd34d0225fd1c9b6073f8f1de2a4ecbe22d6685
romshke <romshkeept+github@gmail.com>
2025-07-26 10:50:43 +0300

    add task: piece of alphabet

--- Topics/Processing strings/Piece of alphabet/src/Main.java
+++ Topics/Processing strings/Piece of alphabet/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        char[] input = scanner.nextLine().toCharArray();
+
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i + 1] - input[i] != 1) {
+                System.out.println("false");
+                return;
+            }
+        }
+
+        System.out.println("true");
     }
 }
