46221297df12a4e1cb680d799c36ec7bb5c4685a
romshke <romshkeept+github@gmail.com>
2025-07-28 21:00:53 +0300

    add task: the longest word

--- Topics/Processing strings/The longest word/src/Main.java
+++ Topics/Processing strings/The longest word/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split(" ");
+        String longestWord = "";
+
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+
+        System.out.println(longestWord);
+
+        scanner.close();
     }
 }
