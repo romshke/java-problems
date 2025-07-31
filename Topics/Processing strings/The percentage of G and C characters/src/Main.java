4bad373142ca3e176a16bcead9ec1f2dfab4a204
romshke <romshkeept+github@gmail.com>
2025-07-31 19:55:11 +0300

    add task: The percentage of G and C characters

--- Topics/Processing strings/The percentage of G and C characters/src/Main.java
+++ Topics/Processing strings/The percentage of G and C characters/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toLowerCase();
+        int count = 0;
+
+        for (char ch : input.toCharArray()) {
+            if (ch == 'c' || ch == 'g') {
+                count++;
+            }
+        }
+
+        double result = (double) count / input.length() * 100;
+        System.out.println(result);
+
+        scanner.close();
     }
 }
