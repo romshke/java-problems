92ba7be2ad40fb74544648160b9bfe802a39f850
romshke <romshkeept+github@gmail.com>
2025-08-02 15:04:02 +0300

    add Create an euphonious word

--- Topics/Processing strings/Create an euphonious word/src/Main.java
+++ Topics/Processing strings/Create an euphonious word/src/Main.java
@@ -3,5 +3,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Scanner scanner = new Scanner(System.in);
+        String vowels = "aeiouyAEIOUY", previousChar = "", input = scanner.nextLine();
+        int count = 0, result = 0;
+
+
+        for (String ch : input.split("")) {
+            if (vowels.contains(ch)) {
+                if (count != 0 && !vowels.contains(previousChar)) {
+                    count = 1;
+                } else {
+                    count++;
+                }
+
+                previousChar = ch;
+            } else {
+                if (count != 0 && vowels.contains(previousChar)) {
+                    count = 1;
+                } else {
+                    count++;
+                }
+
+                previousChar = ch;
+            }
+
+            if (count == 3) {
+                result++;
+                count = 1;
+            }
+        }
+
+        System.out.println(result);
+
+        scanner.close();
     }
 }
