7c6594fc3530b8c66b05699c2ff7df768f9ec744
romshke <romshkeept+github@gmail.com>
2025-08-06 21:03:29 +0300

    add topic Switch expression

---
+++ Topics/Switch expression/Months of the year/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // write your code here
+        String month = switch (scanner.nextInt()) {
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            default -> "error!";
+        };
+
+        System.out.println(month);
+        scanner.close();
+    }
+}
