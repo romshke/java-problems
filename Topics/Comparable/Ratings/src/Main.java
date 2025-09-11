e12c5a373c5b63d8b3462649ea3fdca519d52042
romshke <romshkeept+github@gmail.com>
2025-09-11 23:56:43 +0300

    Add topic Comparable

---
+++ Topics/Comparable/Ratings/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Rating implements Comparable<Rating> {
+    private int upVotes;
+    private int downVotes;
+
+    public int getUpVotes() {
+        return upVotes;
+    }
+
+    public void setUpVotes(int upVotes) {
+        this.upVotes = upVotes;
+    }
+
+    public int getDownVotes() {
+        return downVotes;
+    }
+
+    public void setDownVotes(int downVotes) {
+        this.downVotes = downVotes;
+    }
+
+    @Override
+    public int compareTo(Rating rating) {
+        // write your code here
+        return Integer.compare(this.getUpVotes() - this.getDownVotes(), rating.getUpVotes() - rating.getDownVotes());
+    }
+}
+
+// do not change the code below
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Rating> list = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            int[] votes = Arrays.stream(sc.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            Rating rating = createRating(votes[0], votes[1]);
+            list.add(rating);
+        }
+        Collections.sort(list);
+        Checker.check(list);
+        sc.close();
+    }
+
+    private static Rating createRating(int up, int down) {
+        Rating rating = new Rating();
+        rating.setUpVotes(up);
+        rating.setDownVotes(down);
+        return rating;
+    }
+}
+
+class Checker {
+    static void check(List<Rating> list) {
+        for (int i = 1; i < list.size(); i++) {
+            var curr = list.get(i);
+            var prev = list.get(i  - 1);
+            if (curr.getUpVotes() - curr.getDownVotes() < prev.getUpVotes() - prev.getDownVotes()) {
+                System.out.println("Incorrect sorted order");
+                return;
+            }
+        }
+        System.out.println("Correct sorted order");
+    }
+}
---
+++ Topics/Comparable/Value class/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Age implements Comparable<Age> {
+    private final int value;
+
+    public Age(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public int compareTo(Age other) {
+        return Integer.compare(this.value, other.value);
+    }
+}
+
+// do not change the code below
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Age> list = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .mapToObj(Age::new)
+                .sorted()
+                .collect(Collectors.toList());
+
+        Checker.check(list);
+
+        sc.close();
+    }
+}
+
+class Checker {
+    static void check(List<Age> list) {
+        for (int i = 1; i < list.size(); i++) {
+            var curr = list.get(i);
+            var prev = list.get(i - 1);
+            if (curr.getValue() < prev.getValue()) {
+                System.out.println("Incorrect sorted order");
+                return;
+            }
+        }
+        System.out.println("Correct sorted order");
+    }
+}
