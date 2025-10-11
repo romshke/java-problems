a79749ca105d91b948a3a6fe7ad96ecf1c861af3
romshke <romshkeept+github@gmail.com>
2025-10-12 01:07:47 +0300

    Add topic hashCode() and equals()

---
+++ Topics/hashCode() and equals()/Complex numbers/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+class ComplexNumber {
+
+    private final double re;
+    private final double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ComplexNumber that)) return false;
+        return Double.compare(re, that.re) == 0 && Double.compare(im, that.im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(re, im);
+    }
+}
